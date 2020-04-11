0ec719ffcbed87c08f1ad522a1cba466d4d7ff99
junheethanh0901 <junheethanhkl@gmail.com>
2020-04-11 22:42:44 +0700

    HelloWord

---
+++ app/src/main/java/com/example/huynhnhatthanh_59132265_helloword/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.huynhnhatthanh_59132265_helloword;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    //chup man hinh nay
+    //tool ->SDK ->chup
+    //file -> setting -> version control -> git -> chup
+    //chạy máy ảo -> chụp
+    //4 hình đó
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
